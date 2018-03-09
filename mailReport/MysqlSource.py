b51acf91e37448ff9d5d587bbfc7e5c2bbcaa693
于帅 <yushuai@daojia.com>
2018-03-09 14:16:04 +0800

    完善db链接类

--- mailReport/MysqlSource.py
+++ mailReport/MysqlSource.py
@@ -19,12 +19,16 @@ class MysqlSource(object):
 
         self._conn = pymysql.connect(host=host, user=user, password=passwd, db=db, port=port)
         self._cur = self._conn.cursor()
+        self._data = None
+        self._desc = None
 
     def select(self, sql):
         try:
             self._cur.execute(sql)
-            data = self._cur.fetchall()
-            return data
+            self._data = self._cur.fetchall()
+            self._desc = self._cur.description
+
+            return self._data
 
         except Exception as e:
             if self._cur:
@@ -32,6 +36,12 @@ class MysqlSource(object):
 
             self._conn.close()
         return None
+
+    def get_desc(self):
+        res = []
+        for column in self._desc:
+            res.append(column[0].strip())
+        return res
 
     def close(self):
         if self._cur:
@@ -42,8 +52,12 @@ class MysqlSource(object):
 
 if __name__ == "__main__":
     aaa = MysqlSource("10.37.15.18", "bi_ys", "ak3c3dsi349zdk", db="daojia_dwh", port=3306)
-    for i in aaa.select("select * from information_schema.tables limit 100"):
+    for i in aaa.select("select id,imei from daojia_dwh.t_fact_order limit 100"):
         print(i)
     print('\n' * 4)
+
+    print(aaa.get_desc())
     for i in aaa.select("select * from information_schema.VIEWS limit 100"):
         print(i)
+
+    print(aaa.get_desc())
